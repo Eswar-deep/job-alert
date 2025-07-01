29ee717ca957f07bf7221d1d5283fa0c8bfb724e
Eswar-deep <f20200378@goa.bits-pilani.ac.in>
2025-07-01 15:42:57 -0500

    editted yaml file to prevent workflow from failing

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from scrapers.notify_scraper import check_notify_jobs
 from scrapers.workday_scraper import check_workday_jobs
 
 from notify.telegram_bot import send_telegram
-from storage.db import init_db, store_job_if_new
+from storage.db import store_job_if_new
 
 def notify_if_new(job_id, source, title, company, url):
     if store_job_if_new(job_id, source, title, company, url):
@@ -14,15 +14,14 @@ def notify_if_new(job_id, source, title, company, url):
 
 def run():
     print("[Job Alert] Starting job checks...")
-    init_db()
 
     # Uncomment or comment sources as needed
-    try:
-        print("  → Checking GitHub...")
-        for job in check_github_jobs():
-            notify_if_new(job["id"], "GitHub", job["title"], job["company"], job["url"])
-    except Exception as e:
-        print(f"  [GitHub] Error: {e}")
+    # try:
+    #     print("  → Checking GitHub...")
+    #     for job in check_github_jobs():
+    #         notify_if_new(job["id"], "GitHub", job["title"], job["company"], job["url"])
+    # except Exception as e:
+    #     print(f"  [GitHub] Error: {e}")
 
     # try:
     #     print("  → Checking Notify.Careers...")
@@ -31,12 +30,12 @@ def run():
     # except Exception as e:
     #     print(f"  [Simplify] Error: {e}")
 
-    try:
-        print("  → Checking Notify.Careers...")
-        for job in check_notify_jobs():
-            notify_if_new(job["id"], "Notify", job["title"], job["company"], job["url"])
-    except Exception as e:
-        print(f"  [Notify] Error: {e}")
+    # try:
+    #     print("  → Checking Notify.Careers...")
+    #     for job in check_notify_jobs():
+    #         notify_if_new(job["id"], "Notify", job["title"], job["company"], job["url"])
+    # except Exception as e:
+    #     print(f"  [Notify] Error: {e}")
 
     try:
         print("  → Checking Workday...")
--- storage/db.py
+++ storage/db.py
@@ -3,17 +3,15 @@ from datetime import datetime
 import os
 import certifi
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://eswar:eswar@cluster0.y0qjvno.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.getenv("MONGO_DB", "job_alert")
 COLLECTION_NAME = os.getenv("MONGO_COLLECTION", "jobs")
 
 client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
+print("[MongoDB] Connected to MongoDB!")
 db = client[DB_NAME]
 jobs_collection = db[COLLECTION_NAME]
 
-def init_db():
-    # No need to create a separate index; _id is unique by default
-    pass
 
 def store_job_if_new(job_id, source, title, company, url):
     # Use the job URL as the MongoDB _id for deduplication
